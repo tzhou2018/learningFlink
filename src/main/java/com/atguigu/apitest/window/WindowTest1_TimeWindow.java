d6dcb8610035fb8795b3a98dd8db4a565e03eb79
Solarzhou <tzhou2018@qq.com>
2022-03-10 11:15:34 +0800

    添加窗口函數1

---
+++ src/main/java/com/atguigu/apitest/window/WindowTest1_TimeWindow.java
@@ -0,0 +1,12 @@
+package com.atguigu.apitest.window;
+
+/**
+ * Description TODO
+ * Author zhoutong
+ * Date 2022/3/10 11:11
+ **/
+public class WindowTest1_TimeWindow {
+    public static void main(String[] args) {
+        System.out.printf("hello");
+    }
+}
